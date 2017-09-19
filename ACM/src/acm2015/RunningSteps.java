c946fd15b15b113051bd8cff16fe4d304e348855
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-09-18 22:35:56 -0400

    Add files via upload

---
+++ ACM/src/acm2015/RunningSteps.java
@@ -0,0 +1,17 @@
+package acm2015;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RunningSteps {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner sc = new Scanner(new File("runningsteps.txt"));
+		
+		int input = sc.nextInt();
+		for(int i = 1; i <= input; i++){
+			
+		}
+	}
+}
