ec00325c359fe045d18b84bc85fe693f670631bd
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-10 23:46:46 -0400

    spacing

--- Generics/src/ComparableRectangle.java
+++ Generics/src/ComparableRectangle.java
@@ -2,10 +2,25 @@
 public class ComparableRectangle extends Rectangle 
 implements Comparable<ComparableRectangle>{
 
+	public ComparableRectangle(double width, double height){
+		super(width, height);
+	}
+	
 	@Override
-	public int compareTo(ComparableRectangle arg0) {
+	public int compareTo(ComparableRectangle o) {
 		// TODO Auto-generated method stub
-		return 0;
+		if(getArea() > o.getArea()){
+			return 1;
+		}
+		else if(getArea() > o.getArea()){
+			return -1;
+		}
+		else{
+			return 0;
+		}
 	}
 
+	public String toString(){
+		return super.toString() + "Area: " + getArea();
+	}
 }
