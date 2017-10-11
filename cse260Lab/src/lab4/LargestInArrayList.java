c3c15062683ddbfba096e58048edcd097bd5c8ec
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-11 00:19:21 -0400

    Add files via upload

---
+++ cse260Lab/src/lab4/LargestInArrayList.java
@@ -0,0 +1,14 @@
+package lab4;
+import java.util.*;
+
+public class LargestInArrayList {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static <E extends Comparable<E>> E largest(ArrayList<E> list) {
+		return null;
+		
+	}
+}
